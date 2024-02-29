119924fd70c8080620cdbb7cc535c81bd2cdae61
UHPD Games <uhpdgames@gmail.com>
2024-02-29 14:39:31 +0700

    'upcode2'

--- shared/config.php
+++ shared/config.php
@@ -1,14 +1,14 @@
 <?php
-define('MYADMIN', 'http://localhost/erp/');
-define('MYSITE', 'http://localhost/');
-define('MYSITE_AFFILIATE', 'http://localhost/website2/');
+define('MYADMIN', 'https://ckdvietnam.com/erp');
+define('MYSITE', 'https://ckdvietnam.com/');
+define('MYSITE_AFFILIATE', 'https://ckdvietnam.com/website2/');
 
-if (!defined('ROOT')) define('ROOT', 'D:/WorkSpace-Web/www/website/');
+if (!defined('ROOT')) define('ROOT', '/var/www/vhosts/ckdvietnam.com/httpdocs/');
 
 define('APPNAME', ROOT . 'ctv/');
 
 
-if (!defined('ENDPOINT_CKD_API')) define('ENDPOINT_CKD_API', 'https://admin.ckdvietnam.com/api/');
+if (!defined('ENDPOINT_CKD_API')) define('ENDPOINT_CKD_API', 'https://ckdvietnam.com/erp/api/');
 if (!defined('SHAREDLIBRARIES')) define('SHAREDLIBRARIES', SHAREDPATH . 'libraries/');
 
 define('UPLOAD_PRODUCT_L', 'upload/product/');
@@ -18,5 +18,5 @@ define('UPLOAD_PHOTO_L', 'upload/photo/');
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USER', 'ckdco645_ckdvietnam');
-define('DB_PASS', ')aTbNxQ5@N)w');
+define('DB_PASS', 'I_4389aiy');
 define('DB_NAME', 'ckdco645_ckdvietnam');
